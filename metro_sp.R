2aebb5625d771283e11015c3ddf2fbbffa84f9a5
Jonatha Azevedo <johnmasterchip@gmail.com>
2018-11-07 14:44:19 -0200

    Update metro_sp.R

--- metro_sp.R
+++ metro_sp.R
@@ -1,10 +1,18 @@
-require(dplyr)
-require(rvest)
-library(httr)
-library(xml2)
-library(stringr)
-
+library(dplyr,quietly = TRUE)
+library(rvest, quietly = TRUE)
+library(httr, quietly = TRUE)
+library(xml2, quietly = TRUE)
+library(stringr, quietly = TRUE)
+library(glue, quietly = TRUE)
+library(DBI, quietly = TRUE)
+library(RMySQL, quietly = TRUE)
+options(
+        stringsAsfactors = F
+)
 # URL de requisicao -------------------------------------------------------
+#setwd('~')
+
+
 
 
 url_base = 'http://www.viaquatro.com.br'
@@ -12,18 +20,18 @@ url_base = 'http://www.viaquatro.com.br'
 
 # Requisicao  -------------------------------------------------------------
 
-metro_html <- url_base %>% 
-  httr::GET() %>% 
-  httr::content('text', encoding = 'latin1') %>% 
-  xml2::read_html() %>% 
+metro_html <- url_base %>%
+  httr::GET() %>%
+  httr::content('text', encoding = 'latin1') %>%
+  xml2::read_html() %>%
   rvest::html_nodes('section.operacao')
 
 
 # Requisicao data ---------------------------------------------------------
 
-metro_data = metro_html %>% 
-  rvest::html_nodes('time') %>% 
-  rvest::html_text() %>% 
+metro_data = metro_html %>%
+  rvest::html_nodes('time') %>%
+  rvest::html_text() %>%
   stringr::str_split(pattern = ' ')
 
 # Ajeitando formato data  -------------------------------------------------
@@ -45,11 +53,11 @@ metro_data[1] = as.character(as.Date(metro_data[1],format = '%d/%m/%Y'))
 
 
 df_final = tibble(
-  linha=  metro_html %>% 
-    rvest::html_nodes('h2') %>% 
+  linha=  metro_html %>%
+    rvest::html_nodes('h2') %>%
     rvest::html_text() ,
-  status = metro_html %>% 
-    rvest::html_nodes('span.status') %>% 
+  status = metro_html %>%
+    rvest::html_nodes('span.status') %>%
     rvest::html_text(),
   data   = metro_data[1],
   time   = metro_data[2]
@@ -59,34 +67,49 @@ df_final = tibble(
 # Pegando informacoes das linhas restante ---------------------------------
 
 
-info = metro_html %>% 
-  rvest::html_nodes('div.info') 
+info = metro_html %>%
+  rvest::html_nodes('div.info')
 
 
-linha = metro_html %>% 
-  html_nodes('div.linhas ul li div.info span.title') 
+linha = metro_html %>%
+  html_nodes('div.linhas ul li div.info span.title')
 
-status = metro_html %>% 
-  html_nodes('div.linhas ul li div.info span') 
-  
+status = metro_html %>%
+  html_nodes('div.linhas ul li div.info span')
 
 
-status = status[!(status %in% linha)] %>% 
+
+status = status[!(status %in% linha)] %>%
   html_text()
 
 linha_df = tibble(
-  linha =  metro_html %>% 
-    html_nodes('div.linhas ul li div.info span.title') %>% 
+  linha =  metro_html %>%
+    html_nodes('div.linhas ul li div.info span.title') %>%
     html_text(),
   status = status,
   data   = rep(metro_data[1],12),
   time   = rep(metro_data[2],12)
 )
 
+conn = dbConnect(MySQL(), db = 'metro', user = 'metro_user', password = '123456', host = 'infnetdbistance.cpiyzzzl5acl.sa-east-1.rds.amazonaws.com', port = 3306)
 
+if(!is.null(conn)){
+        message('Conexao estabelicda!')
+        queries = glue("insert into metro.metroSP(linha,status,data,hora) value('{linha_df$linha}','{linha_df$status}','{linha_df$data}','{linha_df$time}')")
+        for(i in 1:length(queries)){try(dbSendQuery(conn = conn, statement = queries[i]),silent = TRUE)}
+        message('Dados inseridos com sucesso!')
+        dbDisconnect(conn)
+}else{
 
 # Agregando tudo ----------------------------------------------------------
-
-
-df_final = rbind(df_final,linha_df)
-View(df_final)
+        if(file.exists('metro_data.csv')){
+                message('Atualiazando os dados!')
+                dados = read.csv2('metro_data.csv')
+                dados = rbind(dados,linha_df)
+                write.csv2(dados, file = 'metro_data.csv', row.names = FALSE)
+        }else{
+                message('Criando o arquivo de dados!')
+                write.csv2(linha_df, file = 'metro_data.csv', row.names = FALSE)
+        }
+}
+message(glue('{Sys.time()} - Done!! (Metro SP - Viaquatro)'))
